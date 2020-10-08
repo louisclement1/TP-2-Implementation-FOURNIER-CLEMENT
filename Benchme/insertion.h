b1870242195a3b6a8201052faa331b0f3a9531d6
louis clement <louis.clement@cpe.fr>
2020-10-08 18:33:57 +0200

    modification .c et .h

---
+++ Benchme/insertion.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   insertion.h
+ * Author: student
+ *
+ * Created on October 8, 2020, 6:28 PM
+ */
+
+#ifndef INSERTION_H
+#define INSERTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void insertion(float tab[], int longueurTab, FILE *fp);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INSERTION_H */
+
